57d60ee765d366701a1ac3aa44602323cfbe7f5d
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-05-07 19:44:27 +0100

    fix: background not covering full body height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import IntractiveComment from "./Store/IntractiveComment";
 
 const page = () => {
   return (
-    <div className="h-fill bg-very-light-gray">
+    <div className="h-full bg-very-light-gray">
       <IntractiveComment>
         <Main />
       </IntractiveComment>
