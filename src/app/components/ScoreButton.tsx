8f68edd42119df137b56404aa66901b51fd211e6
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-05-12 15:18:49 +0100

    fix: correct vote logic to ensure upvote/downvote behavior

--- src/app/components/ScoreButton.tsx
+++ src/app/components/ScoreButton.tsx
@@ -1,6 +1,10 @@
 "use client";
 import React, { useRef, useState } from "react";
 
+const UP = "up";
+const DOWN = "down";
+const NO_VOTE = "";
+
 interface ButtonProps {
   voting: string;
   score: number;
@@ -17,16 +21,31 @@ const ScoreButton = ({
   const [scores, setScores] = useState(score);
   const votes = useRef<string>(voting);
   console.log("votes 🌐:   ", votes.current);
-  console.log('voteReplyValue 😍', voting)
-
+  console.log("voteReplyValue 😍", voting);
 
   const handleIncrement = () => {
-
     console.log("befor testing voests 🔵  ", votes.current);
-    if (votes.current === "up") return;
+
+    if (votes.current === UP) {
+      const newScore = scores - 1;
+      setScores(newScore);
+      const newVote = NO_VOTE;
+      votes.current = newVote;
+      setOnScoreListener?.(newScore, newVote);
+      return;
+    }
+
+    if (votes.current === DOWN) {
+      const newScore = scores + 2;
+      setScores(newScore);
+      const newVote = UP;
+      votes.current = newVote;
+      setOnScoreListener?.(newScore, newVote);
+      return;
+    }
 
     console.log("votes ✅  ", votes.current);
-    const newVote = votes.current === "" ? "up" : "";
+    const newVote = UP;
     votes.current = newVote;
 
     const newScore = scores + 1;
@@ -36,10 +55,25 @@ const ScoreButton = ({
   };
 
   const handleDecrement = () => {
+    if (votes.current === DOWN) {
+      const newScore = scores + 1;
+      setScores(newScore);
+      const newVote = "";
+      votes.current = newVote;
+      setOnScoreListener?.(newScore, newVote);
+      return;
+    }
 
-    if (votes.current === "down") return;
+    if (votes.current === UP) {
+      const newScore = scores - 2;
+      setScores(newScore);
+      const newVote = DOWN;
+      votes.current = newVote;
+      setOnScoreListener?.(newScore, newVote);
+      return;
+    }
 
-    const newVote = votes.current === "" ? "down" : "";
+    const newVote = DOWN;
     votes.current = newVote;
 
     const newScore = scores - 1;
@@ -51,14 +85,18 @@ const ScoreButton = ({
     <div className="bg-very-light-gray text-moderate-blue/55  w-fit font-rubik font-medium  md:text-lg py-1 px-3 md:px-4 rounded-lg flex md:flex-col items-center   gap-4 md:gap-2  ">
       <button
         onClick={handleIncrement}
-        className="cursor-pointer hover:text-moderate-blue"
+        className={`cursor-pointer hover:text-moderate-blue ${
+          voting === "up" ? "text-moderate-blue" : ""
+        }`}
       >
         +
       </button>
       <p className="text-moderate-blue">{scores}</p>
       <button
         onClick={handleDecrement}
-        className="cursor-pointer hover:text-moderate-blue"
+        className={`cursor-pointer hover:text-moderate-blue ${
+          voting === "down" ? "text-moderate-blue" : ""
+        }`}
       >
         -
       </button>
